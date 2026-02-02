c29c45091427f9172e67f94626e319b8731f353f
T125081 <t125081@ed.sus.ac.jp>
2026-02-03 07:00:58 +0900

    色々変更

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-# import plotly.express as px
-# import numpy as np
-# import matplotlib.pyplot as plt
 
 st.title('「食品産業における食品廃棄物等の年間発生量、発生抑制の実施量及び再生利用等実施率」を基にした')
 
@@ -26,7 +23,6 @@ df = pd.read_csv('FEH_00500231_260202222416 copy.csv')
 
 df["total"] = df[["(年)1)食品リサイクル法で規定している用途への実施量【千t】","(年)熱回収の実施量【千t】","(年)減量した量【千t】","(年)2)その他【千t】","(年)廃棄物としての処分量【千t】"]].sum(axis=1)
 
-
 cols = [
     "(年)1)食品リサイクル法で規定している用途への実施量【千t】",
     "(年)熱回収の実施量【千t】",
@@ -47,7 +43,6 @@ df_long["食品産業"] = df_long["食品産業"].astype(str)
 order = df_long["食品産業"].unique().tolist()
 
 with st.container():
-
     st.subheader("各量の全体量とそれの内訳の比較の為の棒グラフ")    
     st.altair_chart(
         alt.Chart(df_long)
@@ -83,10 +78,11 @@ with st.sidebar:
 df_s = df_long[df_long["食品産業"]==fo_lo_kind]
 
 df_s = df_s.copy()
-df_s["割合"] = df_s["量"] / df_s["量"].sum()
+# df_s["割合"] = df_s["量"] / df_s["量"].sum()
 
 with st.container():
-    st.subheader("内訳の円グラフ〈ページ左上の＞をクリックして開くことができるサイドバーの中で、内訳を確認したい食品産業計種類を選択してください。〉")
+    st.subheader("内訳の円グラフ")
+    st.write("〈ページ左上の＞をクリックして開くことができるサイドバーの中で、内訳を確認したい食品産業計種類を選択してください。〉")
     st.altair_chart(
         alt.Chart(df_s)
         .mark_arc()
@@ -104,72 +100,7 @@ with st.container():
     )
 
 
-
-
-
-
-# 栄養表の各データ
-# 年齢別　総合
-
-# df = pd.read_csv('ad_expense_sales.csv')
-
-# with st.sidebar:
-#     st.subheader('抽出条件')
-#     prod_category = st.multiselect('製品カテゴリを選択してください（複数選択可）', 
-#                                    df['prod_category'].unique())
-#     media = st.selectbox('広告媒体を選択してください', 
-#                          df['media'].unique())
-#     st.subheader('色分け')    
-#     color = st.selectbox('分類を選択してください',
-#                       ['性別', '年齢層', '季節'])  
-#     if color == '性別':
-#         color = 'sex'
-#     elif color == '年齢層':
-#         color = 'age'
-#     else:
-#         color = 'season'
-
-# # st.write(f'{color}が選択されました')
-
-# df = df[df['prod_category'].isin(prod_category)]
-# df = df[df['media']==media]
-
-# # st.dataframe(df)
-
-# fig = px.scatter(df, 
-#                  x='ad_expense', 
-#                  y='sales',
-#                  color=color,
-#                  labels={'sales':'売上 (万円)', 'ad_expense':'広告費 (万円)'},
-#                  range_x=[0, df['ad_expense'].max()*1.1],
-#                  range_y=[0, df['sales'].max()*1.1],
-#                  trendline='ols'
-#                 )
-# st.plotly_chart(fig)
-
-# e-Stat（政府統計）のデータを使用
-# CSV形式をそのまま使用してOK
-# （高度なデータ加工は必須ではありません）
-
-# 以下の要素を3つ以上含めること：
+# 以下の要素を１つ以上含めること：
 # ✓ アプリの概要‧目的‧使い方の説明
 # ✓ データの確認（件数、期間、対象など）
-# ✓ データの可視化（最低2種類のグラフ）
-# ✓ 比較や傾向が分かる表示（地域差など）
 # ✓ 可視化結果の簡単な解釈や説明
-
-
-# ✓ サイドバー等で条件を変更できる
-# ✓ 例：都道府県、年、期間、指標など
-# ✓ ユーザ操作でグラフ等の表示が変化する
-# ✓ 単位や補足説明を画面上に明示する
-
-# 【授業で扱っていない部品を探す　公式API Reference を調査】
-# 条件：最低3つ使用 必須
-# 授業コードにない部品を選ぶ
-# 機能的に意味のある使い方をする
-
-# 【他アプリから着想を得る　公式サイトGallery / Explore を参照】
-# 条件：アイデアを取り入れる 必須
-# 優れたデザインや構成を真似ることで、アプリの品質を高めることが目
-# 的です。
